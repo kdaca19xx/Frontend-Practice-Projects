86d7f32febd4e0167e57a486675df2eaa609b061
Kerstin Carson <160202375+kdaca19xx@users.noreply.github.com>
2024-10-24 13:08:46 -0500

    Background Picker a la 100Devs and WesBos

---
+++ background picker/js/main.js
@@ -0,0 +1,43 @@
+document.getElementById('red').onclick = partyRed
+document.getElementById('orange').onclick = partyOrange
+document.getElementById('yellow').onclick = partyYellow
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('brown').onclick = partyBrown
+
+
+function partyRed() {
+  document.querySelector('body').style.backgroundColor = 'red'
+  document.querySelector('body').style.color = 'black'
+}
+
+function partyOrange() {
+  document.querySelector('body').style.backgroundColor = 'orange'
+  document.querySelector('body').style.color = 'black'
+}
+
+function partyYellow() {
+  document.querySelector('body').style.backgroundColor = 'yellow'
+  document.querySelector('body').style.color = 'black'
+}
+
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'green'
+  document.querySelector('body').style.color = 'black'
+}
+
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'blue'
+  document.querySelector('body').style.color = 'black'
+}
+
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'purple'
+  document.querySelector('body').style.color = 'black'
+}
+
+function partyBrown() {
+  document.querySelector('body').style.backgroundColor = 'brown'
+  document.querySelector('body').style.color = 'black'
+}
